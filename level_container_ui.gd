41d011d2082b4916adcca37a8dea1d986bb6f0cd
excadrill42 <aaronfrisch05@gmail.com>
2026-02-21 14:21:42 -0500

    Im scared

---
+++ level_container_ui.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+var current_xp = 0.0
+var xp_needed = 5.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	call_deferred("_update_label")
+	$Progressbar.max_value = xp_needed
+
+func _update_label() -> void:
+	$Label.text = "Level: " + str(Global.level)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func task_completed():
+	pass
