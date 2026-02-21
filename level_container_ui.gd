ac135bc2d2185e48d189ac7d48335dcfce5878b1
excadrill42 <aaronfrisch05@gmail.com>
2026-02-21 15:11:25 -0500

    Level Update

--- level_container_ui.gd
+++ level_container_ui.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
-var current_xp = 0.0
-var xp_needed = 5.0
+var current_xp = 0
+var xp_needed = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,20 +16,17 @@ func _process(delta: float) -> void:
 	pass
 
 func task_completed():
-	current_xp =+ Global.task_length_input
+	current_xp += Global.task_length_input
 	if (current_xp >= xp_needed):
-		level_up(current_xp - xp_needed)
+		level_up()
 	else:
 		$ProgressBar.value = current_xp
 
-func level_up(new_current_xp):
-	Global.level =+ 1
-	current_xp = new_current_xp
+func level_up():
+	Global.level += current_xp / xp_needed
+	current_xp = current_xp % xp_needed
 	$Label.text = "Level: " + str(Global.level)
 	$ProgressBar.value = current_xp
-	
-	
-
 
 func _on_example_pressed() -> void:
 	task_completed()
