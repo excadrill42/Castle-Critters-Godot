dbff775cf5f1b9aa79479762024fb7a7fd58edf1
ps741 <ps741@cairn.edu>
2026-02-21 15:10:58 -0500

    things

--- timer_script.gd
+++ timer_script.gd
@@ -3,29 +3,30 @@ extends Control
 @onready var timer = $CountdownTimer
 @onready var time_label = $TimeLabel
 
-var task_length
-var tasks_completed_value
-var points_to_be
+var minutes
 var points = 1
+var points_to_be
 
 func _ready():
-	task_length = 5
-	
-	if task_length <= 0:
-		task_length = 5
-	
+	minutes = Global.task_length_input  # must be a number, not a node
+
+	if minutes <= 0:
+		minutes = 1
+
+	points_to_be = round(minutes * 0.333)
+
+	timer.one_shot = true
 	timer.timeout.connect(_on_timer_finished)
-	timer.start(task_length)
+	timer.start(minutes * 60)
 
 func _process(delta):
 	if not timer.is_stopped():
-		time_label.text = str(ceil(timer.time_left))
-
-	tasks_completed_value = round(task_length * 0.333)
-	points_to_be = tasks_completed_value
-
-
+		var total_seconds = int(timer.time_left)
+		var mins = total_seconds / 60
+		var secs = total_seconds % 60
+		time_label.text = "%02d:%02d" % [mins, secs]
 
 func _on_timer_finished():
 	points += points_to_be
+	time_label.text = "Done!"
 	print("Points:", points)
